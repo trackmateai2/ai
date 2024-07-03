d4492abf77f6da0369e71ab9ec0faffc437c2bed
trackmateai2 <trackmateai2@gmail.com>
2024-07-03 23:14:58 +0200

    Add files via upload

---
+++ loginScript.js
@@ -0,0 +1,88 @@
+
+function getAccess(){
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+        if(username=="Masele"&&password=="Mypass"){
+            
+            window.location.href = "appHome.html";
+        }
+         
+    if(username=="edwinnkgorosang779@gmail.com"&&password=="nkgorosang8209"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="amemodibo@gmail.com"&&password=="Stalin1962"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="OCPHAPOGO"&&password=="Phapogo#87"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="Bethel"&&password=="botho728"){
+            
+            window.location.href = "html2.html";
+        }
+     if(username=="segotsoreuben@gmail.com"&&password=="Rosinahsegotso87"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="smotswakhumo@gmail.com"&&password=="74420750hope"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="daphneymompedi@gmail.com"&&password=="Incorrect"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="williamramatshwan1@gmail.com"&&password=="Will2023"){
+            
+            window.location.href = "html2.html";
+        }
+   if(username=="smotswakhumo@gmail.com"&&password=="74420750hope"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="Stimslo"&&password=="PLayerHater1952"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="lonerankgomo@gmail.com"&&password=="Rankgomo"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="mathesbf@gmail.com"&&password=="S3@bello"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="nkisasametsi@gmail.com"&&password=="Chidodoh"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="niahopetshuma2002@gmail.com"&&password=="75600242Nigel"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="ogaatlale@gmail.com"&&password=="Advo2012"){//Gabriel
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="batshwarijealous@gmail.com"&&password=="Obonetse11"){
+            
+            window.location.href = "html2.html";
+        }
+    if(username=="bonyongomc@gmail.com"&&password=="Pa55word1"){
+
+            window.location.href = "html2.html";
+        }
+   if(username=="kaevarua92@gmail.com"&&password=="77172939"){
+
+            window.location.href = "html2.html";
+        }
+
+    
+    
+ }
+
---
+++ loginScriptApp.js
@@ -0,0 +1,394 @@
+function getAccess(){
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+
+
+    var loginLay= document.getElementById("LoginLay");
+    var mainLay= document.getElementById("MainLay");
+    
+    var email= document.getElementById("tittleMain");
+
+    
+    
+
+
+
+    if(username=="Masele"&&password=="Mypass"){
+            loginLay.style.display=("none");
+            mainLay.style.display=("block");
+            
+            email.innerHTML=(username);
+        }
+         
+    if(username=="edwinnkgorosang779@gmail.com"&&password=="nkgorosang8209"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="amemodibo@gmail.com"&&password=="Stalin1962"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="OCPHAPOGO"&&password=="Phapogo#87"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="Bethel"&&password=="botho728"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     if(username=="segotsoreuben@gmail.com"&&password=="Rosinahsegotso87"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="smotswakhumo@gmail.com"&&password=="74420750hope"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="daphneymompedi@gmail.com"&&password=="Incorrect"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="williamramatshwan1@gmail.com"&&password=="Will2023"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="smotswakhumo@gmail.com"&&password=="74420750hope"){
+    loginLay.style.display=("none");
+    mainLay.style.display=("block");
+    
+    email.innerHTML=(username);
+        }
+    if(username=="Stimslo"&&password=="PLayerHater1952"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="lonerankgomo@gmail.com"&&password=="Rankgomo"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="mathesbf@gmail.com"&&password=="S3@bello"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="nkisasametsi@gmail.com"&&password=="Chidodoh"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="niahopetshuma2002@gmail.com"&&password=="75600242Nigel"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="ogaatlale@gmail.com"&&password=="Advo2012"){//Gabriel
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="batshwarijealous@gmail.com"&&password=="Obonetse11"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="bonyongomc@gmail.com"&&password=="Pa55word1"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+
+    if(username=="bonyongomc"&&password=="bonyongomc"){
+            loginLay.style.display=("none");
+            mainLay.style.display=("block");
+            
+            email.innerHTML=(username);
+        }
+   if(username=="ashrafprofessor30@gmail.com"&&password=="77712323"){
+
+            loginLay.style.display=("none");
+            mainLay.style.display=("block");
+            
+            email.innerHTML=(username);
+        }
+    
+   if(username=="klaytonseltok@gmail.com"&&password=="Afiwaselato2016"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     
+   if(username=="mmopsconsultants@gmail.com"&&password=="mmops123"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+
+   if(username=="ftando8@gmail.com"&&password=="FrancoT1994!"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    
+   if(username=="slamd.mogotsi@gmail.com"&&password=="Mmolimi.1995"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="gdonaldompatile@gmail.com"&&password=="901019727G"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="seizsoka5694@gmail.com"&&password=="Seiz569493%"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="vincent.madome7@gmail.com"&&password=="Vineyard88"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="motsumigorata@gmail.com"&&password=="Tinashe2017*"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="zibisot@gmail.com"&&password=="Ishmaeltbg@12"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="moatshekingbolt@gmail.com"&&password=="75119359"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="frankburu77@gmail.com"&&password=="Quinton16"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="tgabakgore@gmail.com"&&password=="72632349"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     if(username=="seemabokang@yahoo.com"&&password=="Seema@1990"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="stiloryinvest@gmail.com"&&password=="Seema@1990"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="gmmoaneng@gmail.com"&&password=="72646279"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+if(username=="sales.ashflames@gmail.com"&&password=="Modikwa1987"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="justintsapo@gmail.com"&&password=="@officialjustin93"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="kennygee148@gmail.com"&&password=="Kenosisam123"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="pakomanja@gmail.com"&&password=="Pakoandmanja24"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="neosystems.follow@gmail.com"&&password=="T3chno@9081.!#"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="neosystems.follow@gmail.com"&&password=="T3chno@9081.!#"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="mphojim614@gmail.com"&&password=="Nelson2020"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     if(username=="tgabakgore@gmail.com"&&password=="72632349"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="vusatshegofatso@gmail.com"&&password=="Vusa@12345"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="kelediyakgotlal@gmail.com"&&password=="03011988"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+
+    if(username=="tsaonekelediyakgotla@gmail.com"&&password=="botswana"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="Edditlets@gmail.com"&&password=="71744642"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+  if(username=="kelediyakgotlaboi@gmail.com"&&password=="01343011936"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+  if(username=="eddietlets@gmail.com"&&password=="eddietlets"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="sengwekelediyakgotla@gmail.com"&&password=="Sengwe08"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="bamerakolanyane@gmail.com"&&password=="933018518"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="tefgeo@gmail.com"&&password=="Bgeof922"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="dkithya@gmail.com"&&password=="JoyMulei@2012"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="gideon2203@gmail.com"&&password=="Gedeon1991"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="taimoss@gmail.com"&&password=="72511797"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     if(username=="phlpkhaya@gmail.com"&&password=="MakPhlp@84"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="emmahdt@gmail.com"&&password=="326419914"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="sirserwalo@gmail.com"&&password=="Onameditse"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     if(username=="thabangthutogidion@gmail.com"&&password=="Gidion@1998"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);  
+     }
+    if(username=="mphojim614@gmail.com"&&password=="Ney2020"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);  
+     }
+if(username=="nesigwele@gmail.com"&&password=="Neo82sig"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);  
+     }
+  
+    
+ }
