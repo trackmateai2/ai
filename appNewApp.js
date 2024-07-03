43bc5f8aa79132913989d616db33539a1c83439c
trackmateai2 <trackmateai2@gmail.com>
2024-07-03 23:07:24 +0200

    Add files via upload

---
+++ appNewApp.js
@@ -0,0 +1,34 @@
+
+
+function startTracking(){
+     window.location.href=("appNewAppTracking.html");
+}
+function goHomee(){
+          var LoginLay = document.getElementById("LoginLay");
+          var MainLay = document.getElementById("MainLay");
+          
+          LoginLay.style.display=("none");
+          MainLay.style.display=("Block");
+}
+function back(){
+    window.location.href=("appNewAppMain.html");
+}
+function showCard(){
+    card33=document.getElementById("card3");
+    card3.style.display=("block");
+}
+function disclaimer(){
+    window.location.href=("disclaimer.html");
+}
+function privacyPolicy(){
+    window.location.href=("privacyPolicy.html");
+}
+
+
+
+
+
+
+
+
+
---
+++ appNewAppFeedback.js
@@ -0,0 +1,136 @@
+
+function feedBack(){
+    
+                card3=document.getElementById("card3");
+                card3.style.display=("none");
+    
+    var username= document.getElementById("emailVerify").value;
+    
+    var myMainLay = document.getElementById("MainLay2");
+    var myInfoLay = document.getElementById("InfoLay");
+
+            myMainLay.style.display=("none");
+            myInfoLay.style.display=("block");
+    
+    var mainInfo = document.getElementById("mainInfo");
+    var moreInfo = document.getElementById("moreInfo");
+
+    
+                phoneImageLay= document.getElementById("imageLay")
+                phoneImage= document.getElementById("phoneImage")
+      
+
+   
+ if (username=="Daveyut@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro.jpg";
+    
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352841110948057</font><br/>  Model: iPhone 11 Pro Max(A2161)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	iOS 17<br/>Chipset	Apple A13 Bionic<br/>Number of CPU Cores	6<br/>RAM	4 GB<br/>Width	77.8 mm<br/>Height	158.0 mm<br/>Thickness	8.1 mm<br/>Weight	226.0 g<br/>DisplayType	OLED<br/>Diagonal	6.5 inches<br/>Resolution	1242x2688 px<br/>Screen-to-body ratio	84.5 %<br/>Pixel density	456 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+
+ if (username=="Nivesaro@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15.jpg";
+    
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 359282705344506</font><br/>  Model: APPLE iPhone 15(A3090)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	iOS 17<br/>Chipset	Apple A16 Bionic<br/>Number of CPU Cores	2<br/>RAM	6 GB<br/>Width	71.6 mm<br/>Height	147.6 mm<br/>Thickness	7.8 mm<br/>Weight	171.0 g<br/>DisplayType	RETINA<br/>Diagonal	6.1 inches<br/>Resolution	1179x2556 px<br/>Screen-to-body ratio	86.4 %<br/>Pixel density	461 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+
+ if (username=="ashrafprofessor30@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15.jpg";
+    
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 359282705344506</font><br/>  Model: iPhone 15(A3090)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana   ");
+        moreInfo.innerHTML=("Operating System	iOS 17<br/>Chipset	Apple A16 Bionic<br/>Number of CPU Cores	2<br/>RAM	6 GB<br/> Width	71.6 mm<br/>Height	147.6 mm<br/>Thickness	7.8 mm<br/>Weight	171.0 g<br/>DisplayType	RETINA<br/>Diagonal	6.1 inches<br/>Resolution	1179x2556 px<br/>Screen-to-body ratio	86.4 %<br/>Pixel density	461 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+    if (username=="nktheboy89@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/6/C/ISX6FT-d.jpg";
+    
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352859293147396</font><br/>  Model: Spark 10 Pro<br/>Brand: TECNO <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Operating System	Android 13<br/>Chipset	Mediatek Helio G88<br/>Number of CPU Cores	2<br/>RAM	8 GB<br/>Width	76.2 mm<br/>Height	168.4 mm<br/>Thickness	8.4 mm<br/>Weight	208.0 g<br/>DisplayType	LCD_IPS<br/>Diagonal	6.8 inches<br/>Resolution	1080x2460 px<br/>Screen-to-body ratio	85.9 %<br/>Pixel density	395 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+    if (username=="ramapulana@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/n/R/UxcXxI-d.jpg";
+    
+        mainInfo.innerHTML=("  STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356061285119931</font><br/>  Model: Camon 19 Pro<br/>Brand: TECNO <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	Android 12<br/>Chipset	MediaTek Dimensity 810 MT6833V<br/>Number of CPU Cores	2<br/>RAM	8 GB<br/>idth	74.6 mm<br/>Height	166.8 mm<br/>Thickness	8.58 mm<br/>DisplayType	LCD_IPS<br/>Diagonal	6.8 inches<br/>Resolution	1080x2460 px<br/>Screen-to-body ratio	88.2 %<br/>Pixel density	395 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch  ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+ if (username=="gofiwaseabelo99@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a14-4g.jpg";
+    
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 357093537218889</font><br/>  Model: Galaxy A14 5G(SM-A145P)<br/>Brand: SAMSUNG <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana   ");
+        moreInfo.innerHTML=(" Model Info: MOBILE SM-A145P/DS BLACK MEA<br/>Search Term: 357093537218889<br/>IMEI 1: 357093537218889<br/>Serial Number: R8YW50R73JH<br/>Model Desc: Galaxy A14<br/>Model Name: SM-A145P/DS<br/>Model Number: SM-A145PZKDMEA<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 19-07-2026<br/>Production location: Korea SEC<br/>Estimated Production Date: 19-07-2024<br/>Country: Botswana<br/>Carrier: Factory Unlocked ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+ if (username=="neoboy71@gmail.com"){
+            window.location.href=("resultsNeoboy86519.html");
+   }
+if (username=="Burugaha@gmail.com"){
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg";
+    
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 354703755842276</font><br/>  Model: iPhone 12 Pro Max(A2342)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Operating System	iOS 17<br/>Chipset	Apple A14 Bionic<br/>RAM	6 GB<br/>Width	78.09 mm<br/>Height	160.84 mm<br/>Thickness	7.39 mm<br/>Weight	228.0 g<br/>DisplayType	OLED<br/>Diagonal	6.7 inches<br/>Resolution	1824x2778 px<br/>Pixel density	496 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+if (username=="seeletsotuduetso@gmail.com"){
+            window.location.href=("resultsSeeletso35709.html");
+   }
+    
+if (username=="itseng.kgetse@gmail.com"){
+            window.location.href=("resultsItengKgetse.html");
+   }
+
+
+
+
+
+    
+    /*
+   if (username=="gofiwaseabelo99@gmail.com"){
+          var x = document.getElementById("snackbar");
+          x.className = "show";
+          setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4000);
+   }
+    if (username=="gmasele15@gmail.com"){
+          var x = document.getElementById("snackbar");
+          x.className = "show";
+          setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4000);
+   } 
+   */
+    
+}
+
+
+
+
+
+
+
+
+
+
+
