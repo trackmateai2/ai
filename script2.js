2d5a89b519b8113d2d8d64dce612b039d4b0c3da
trackmateai2 <trackmateai2@gmail.com>
2024-07-03 23:16:41 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+        
+function viewInfo(){
+            myCardPhone0= document.getElementById("cardPhoneId");
+            myCardPhone0.style.display=("block");
+}
+
+function proceed(){
+
+    tef= document.getElementById("tef");
+    tefInfo = tef.value;
+    
+     
+    if(tefInfo=="Advo2012"){
+       header= document.getElementById("headerId");
+        header.style.display="block";
+    }
+
+}
