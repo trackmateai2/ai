87bac4a1b2b3456da0aded7755aa90220b877e2a
trackmateai2 <trackmateai2@gmail.com>
2024-07-08 14:33:28 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -21,28 +21,7 @@ function feedBack(){
       
 
    
- if (username=="resadaet@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 354703755842276</font><br/>  Model: iPhone 12 Pro Max(A2342)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=("Operating System	iOS 17<br/>Chipset	Apple A14 Bionic<br/>RAM	6 GB<br/> Width	78.09 mm<br/>Height	160.84 mm<br/>Thickness	7.39 mm<br/>Weight	228.0 g <br/> DisplayType	OLED<br/>Diagonal	6.7 inches<br/>Resolution	1824x2778 px<br/>Pixel density	496 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch    ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
-
-
- if (username=="ashrafprofessor30@gmail.com"){
-           phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 354703755842276</font><br/>  Model: iPhone 12 Pro Max(A2342)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=("Operating System	iOS 17<br/>Chipset	Apple A14 Bionic<br/>RAM	6 GB<br/> Width	78.09 mm<br/>Height	160.84 mm<br/>Thickness	7.39 mm<br/>Weight	228.0 g <br/> DisplayType	OLED<br/>Diagonal	6.7 inches<br/>Resolution	1824x2778 px<br/>Pixel density	496 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch    ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
+ 
 if (username=="galanimorima97@gmail.com"){
            phoneImageLay.style.display=("block");
             phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/tecno-spark-go-2022.jpg";
@@ -55,6 +34,17 @@ if (username=="galanimorima97@gmail.com"){
    }
 
     
+if (username=="tmotlhaleemang@gmail.com"){
+           phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg";
+    
+        mainInfo.innerHTML=("  STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 355317932760894</font><br/>  Model: iPhone 13(A2633)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("  Operating System	iOS 17<br/>Chipset	Apple A15 Bionic<br/>Number of CPU Cores	6<br/>RAM	6 GB<br/>Width	71.5 mm<br/>Height	146.7 mm<br/>Thickness	7.7 mm<br/>Weight	174.0 g<br/>DisplayType	RETINA<br/>Diagonal	6.1 inches<br/>Resolution	1170x2532 px<br/>Pixel density	457 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+   }
+
     
 
 
