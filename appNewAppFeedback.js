b63b15c54e51f490a3251a4e61cc642380f13da2
trackmateai2 <trackmateai2@gmail.com>
2024-08-12 11:11:48 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -43,15 +43,8 @@ function feedBack(){
         moreInfo.style.display=("block");
    }
   
- if (username=="gofiwaseabelo99@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14.jpg";
-    
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356691196956474</font><br/>  Model:  iPhone 14 (A2649)<br/>Brand: Apple<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana  ");
-        moreInfo.innerHTML=("Operating System	iOS 17<br/>Chipset	Apple A15 Bionic<br/>Number of CPU Cores	2<br/>RAM	6 GB<br/>Width	71.5 mm<br/>Height	146.7 mm<br/>Thickness	7.8 mm<br/>Weight	172.0 g<br/>DisplayType	RETINA<br/>Diagonal	6.1 inches<br/>Resolution	1170x2532 px<br/>Screen-to-body ratio	86.0 %<br/>Pixel density	460 ppi<br/>Colors	1073M<br/>Other display Features	, Display refresh rate: HDR10 ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
+ if (username=="Ashrafprofessor30@gmail.com"){
+           alert("Invalid IMEi number-86794905867639");
    }
 if (username=="gmasele15@gmail.com"){
            window.location.href=("resultsRati.html");
