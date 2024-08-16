419a6dfa5eb98c35bedd08d7375d5305e22abdb4
trackmateai2 <trackmateai2@gmail.com>
2024-08-16 12:09:19 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -29,9 +29,6 @@ function feedBack(){
    }
   
     
- if (username=="ratiketlogetswe@gmail.com"){
-            window.location.href=("resultsRati.html");
-   }
  if (username=="mojwadigaongalelwe@gmail.com"){
             phoneImageLay.style.display=("block");
             phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/h/D/c30ozb-d.jpg";
@@ -49,15 +46,8 @@ function feedBack(){
 if (username=="gmasele15@gmail.com"){
            window.location.href=("resultsRati.html");
    }
-if (username=="moalosi@cnl.co.bw"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/r/c/324qM6-d.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356844112290235</font><br/>  Model: iPhone SE 2020(A2275)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana");
-        moreInfo.innerHTML=(" Operating System	iOS<br/>Chipset	Apple A13 Bionic APL1W85<br/>Number of CPU Cores	6<br/>RAM	3 GB<br/> Width	67.3 mm<br/>Height	138.4 mm<br/>Thickness	7.3 mm<br/>Weight	148.0 g<br/>DisplayType	LCD_IPS<br/>Diagonal	4.7 inches<br/>Resolution	750x1334 px<br/>Screen-to-body ratio	65.3 %<br/>Pixel density	326 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch, Scratch resistant ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
+if (username=="Ashrafprofessor30@gmail.com"){
+           Alert("Your Account Has Been Blocked!");
    }
 
     
