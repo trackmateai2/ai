e9f9627dd758fdcef78452c13d1510afa1516c27
trackmateai2 <trackmateai2@gmail.com>
2024-08-26 14:06:31 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -30,14 +30,7 @@ function feedBack(){
   
     
  if (username=="flockie2002@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/x/9/Eq3lPN-d.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 357492755696661</font><br/>  Model: Galaxy A05 (SM-A055F)<br/>Brand: Samsung<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Model Info: MOBILE SM-A055F/DS BLACK AFC<br/>Search Term: 357492755696661<br/>IMEI 1: 357492755696661<br/>Serial Number: R92X20V6XBZ<br/>Model Desc: Galaxy A05<br/>Model Name: SM-A055F/DS<br/>Model Number: SM-A055FZKDAFC<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 01-03-2026<br/>Production location: Korea SEC<br/>Production Date: 29-02-2024 ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
+          window.location.href=("resultsFlockie35749.html");
    }
   
  if (username=="tntmots@gmail.com"){
