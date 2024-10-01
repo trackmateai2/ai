d4bebb7cbb0e1e6b8c4871eba1ae3aa87dae7204
trackmateai2 <trackmateai2@gmail.com>
2024-10-01 13:28:34 +0200

    Update loginFirebase.js

--- loginFirebase.js
+++ loginFirebase.js
@@ -31,9 +31,7 @@ function signUpFirebase(){
 function signInFirebase(){
 
 
-  var x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
 
   
     
